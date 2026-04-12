de43a68610da8a73eb78f934eefe247e3545b63e
Woundwort124 <shadow.mortis.7@outlook.com>
2026-04-13 01:16:18 +0800

    feat. test env

--- User/ch32v30x_it.c
+++ User/ch32v30x_it.c
@@ -1,14 +1,14 @@
 /********************************** (C) COPYRIGHT *******************************
-* File Name          : ch32v30x_it.c
-* Author             : WCH
-* Version            : V1.0.0
-* Date               : 2024/03/05
-* Description        : Main Interrupt Service Routines.
-*********************************************************************************
-* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
-* Attention: This software (modified or not) and binary are used for 
-* microcontroller manufactured by Nanjing Qinheng Microelectronics.
-*******************************************************************************/
+ * File Name          : ch32v30x_it.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2024/03/05
+ * Description        : Main Interrupt Service Routines.
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
 #include "ch32v30x_it.h"
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
@@ -23,9 +23,8 @@ void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
  */
 void NMI_Handler(void)
 {
-  while (1)
-  {
-  }
+	while (1) {
+	}
 }
 
 /*********************************************************************
@@ -37,10 +36,7 @@ void NMI_Handler(void)
  */
 void HardFault_Handler(void)
 {
-  NVIC_SystemReset();
-  while (1)
-  {
-  }
+	NVIC_SystemReset();
+	while (1) {
+	}
 }
-
-
